2fd211628e164fca00cf802b233822288ea68b0c
LUO12826 <luohuizhou@hotmail.com>
2023-07-03 22:18:53 +0800

    This commit is only for updating the commit message.

--- quickjs-test.sh
+++
@@ -1,8 +0,0 @@
-echo "run test_builtin"
-bin/es test/quickjs/test_builtin.js
-echo "run test_closure"
-bin/es test/quickjs/test_closure.js
-echo "run test_language"
-bin/es test/quickjs/test_language.js
-echo "run test_loop"
-bin/es test/quickjs/test_loop.js
