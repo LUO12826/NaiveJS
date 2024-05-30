0eb1dd5e5a58c60a279143a576afc6a97ea6f0db
LUO12826 <luohuizhou@hotmail.com>
2024-05-30 16:09:26 -0400

    update test files.

--- test_files/test_typescript_mod.js
+++ test_files/test_typescript_mod.js
@@ -4760,41 +4760,41 @@ var JSON2 = {
             });
         };
     }
-    // if(typeof JSON2.parse !== 'function') {
-    //     JSON2.parse = function (text, reviver) {
-    //         var j;
-    //         function walk(holder, key) {
-    //             var k = null, v, value = holder[key];
-    //             if(value && typeof value === 'object') {
-    //                 for(k in value) {
-    //                     if(Object.prototype.hasOwnProperty.call(value, k)) {
-    //                         v = walk(value, k);
-    //                         if(v !== undefined) {
-    //                             value[k] = v;
-    //                         } else {
-    //                             delete value[k];
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //             return reviver.call(holder, key, value);
-    //         }
-    //         text = String(text);
-    //         cx.lastIndex = 0;
-    //         if(cx.test(text)) {
-    //             text = text.replace(cx, function (a) {
-    //                 return '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-    //             });
-    //         }
-    //         if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
-    //             j = eval('(' + text + ')');
-    //             return typeof reviver === 'function' ? walk({
-    //                 '': j
-    //             }, '') : j;
-    //         }
-    //         throw new SyntaxError('JSON.parse');
-    //     };
-    // }
+    if(typeof JSON2.parse !== 'function') {
+        // JSON2.parse = function (text, reviver) {
+        //     var j;
+        //     function walk(holder, key) {
+        //         var k = null, v, value = holder[key];
+        //         if(value && typeof value === 'object') {
+        //             for(k in value) {
+        //                 if(Object.prototype.hasOwnProperty.call(value, k)) {
+        //                     v = walk(value, k);
+        //                     if(v !== undefined) {
+        //                         value[k] = v;
+        //                     } else {
+        //                         delete value[k];
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //         return reviver.call(holder, key, value);
+        //     }
+        //     text = String(text);
+        //     cx.lastIndex = 0;
+        //     if(cx.test(text)) {
+        //         text = text.replace(cx, function (a) {
+        //             return '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+        //         });
+        //     }
+        //     if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+        //         j = eval('(' + text + ')');
+        //         return typeof reviver === 'function' ? walk({
+        //             '': j
+        //         }, '') : j;
+        //     }
+        //     throw new SyntaxError('JSON.parse');
+        // };
+    }
 })());
 var TypeScript;
 (function (TypeScript) {
@@ -25776,7 +25776,6 @@ function runTypescript() {
            directoryExists: function (path) { return false; },
            resolvePath: function (path) { return path; }
   });
-  
   if (parseErrors.length != 192 && parseErrors.length != 193) {
     throw new Error("Parse errors.");
   }
